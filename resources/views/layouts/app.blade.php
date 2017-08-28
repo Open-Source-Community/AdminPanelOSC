4592c1d53fbcfe247ad816d21576b692712553a2
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-08-28 21:52:30 +0200

    tmam keda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Admin Panel ( {{ Auth::user()->name }} )</title>
+    <title>Admin Panel</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
