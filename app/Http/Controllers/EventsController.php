4fa8cab52e84e0f41913115a937089b8eac5f5f9
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-09-15 19:41:28 +0200

    sorted

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -37,7 +37,19 @@ class EventsController extends Controller
     public function index2()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Saturday'])->get();
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Saturday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Saturday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Saturday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Saturday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Saturday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
 
             return view('showallinterviews', compact('users_data'));
         } else {
@@ -47,7 +59,19 @@ class EventsController extends Controller
     public function index3()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Sunday'])->get();
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Sunday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Sunday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Sunday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Sunday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Sunday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
 
             return view('showallinterviews', compact('users_data'));
         } else {
@@ -57,7 +81,19 @@ class EventsController extends Controller
     public function index4()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Monday'])->get();
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Monday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Monday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Monday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Monday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Monday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
 
             return view('showallinterviews', compact('users_data'));
         } else {
@@ -67,7 +103,19 @@ class EventsController extends Controller
     public function index5()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Tuesday'])->get();
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Tuesday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Tuesday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Tuesday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Tuesday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Tuesday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
 
             return view('showallinterviews', compact('users_data'));
         } else {
@@ -77,8 +125,20 @@ class EventsController extends Controller
     public function index6()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Wednesday'])->get();
-
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Wednesday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Wednesday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Wednesday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Wednesday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Wednesday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
+            
             return view('showallinterviews', compact('users_data'));
         } else {
             return redirect('/login');
@@ -87,7 +147,19 @@ class EventsController extends Controller
     public function index7()
     {
         if (Auth::user() != null) {
-            $users_data = Form::where(['day' => 'Thursday'])->get();
+            $time1 = '10 am to 11.30 am';
+            $time2 = '11.30 am to 1 pm';
+            $time3 = '1 pm to 2.30 pm';
+            $time4 = '2.30 pm to 4 pm';
+            $time5 = '4 pm to 5.30 pm';
+            
+            $users_data1 = Form::where(['day' => 'Thursday'])->where(['time' => $time1])->get();
+            $users_data2 = Form::where(['day' => 'Thursday'])->where(['time' => $time2])->get();
+            $users_data3 = Form::where(['day' => 'Thursday'])->where(['time' => $time3])->get();
+            $users_data4 = Form::where(['day' => 'Thursday'])->where(['time' => $time4])->get();
+            $users_data5 = Form::where(['day' => 'Thursday'])->where(['time' => $time5])->get();
+            
+            $users_data = $users_data1->merge($users_data2)->merge($users_data3)->merge($users_data4)->merge($users_data5);
 
             return view('showallinterviews', compact('users_data'));
         } else {
