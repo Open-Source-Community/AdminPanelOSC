2cdc4bc3e39687a5ecaa2fafe163ce32fe28e2b8
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2018-08-15 02:37:53 +0200

    fixed another issue with redundancy of select items

--- resources/views/Admin/recruitment/edit.blade.php
+++ resources/views/Admin/recruitment/edit.blade.php
@@ -54,82 +54,141 @@
 
                                     <div class="form-group form-md-line-input">
                                         <select required name="year" class="form-control">
-                                            <option selected
-                                                    value="{{$item->year}}">{{$item->year}}
+                                            <option @if($item->year=="First Year") selected @endif
+                                            value="First Year">First Year
                                             </option>
-                                            <option value="First Year">First Year</option>
-                                            <option value="Second Year">Second Year</option>
-                                            <option value="Third Year">Third Year</option>
-                                            <option value="Fourth Year">Fourth Year</option>
+                                            <option @if($item->year=="Second Year") selected @endif
+                                            value="Second Year">Second Year
+                                            </option>
+                                            <option @if($item->year=="Third Year") selected @endif
+                                            value="Third Year">Third Year
+                                            </option>
+                                            <option @if($item->year=="Fourth Year") selected @endif
+                                            value="Fourth Year">Fourth Year
+                                            </option>
                                         </select>
                                         <label for="year">Year</label>
                                     </div>
 
                                     <div class="form-group form-md-line-input">
                                         <select required name="first_committee" class="form-control">
-                                            <option selected
-                                                    value="{{$item->first_committee}}">{{$item->first_committee}}
+                                            <option @if($item->first_committee=="Linux") selected @endif
+                                            value="Linux">Linux
                                             </option>
-                                            <option value="Linux">Linux</option>
-                                            <option value="Blender">Blender</option>
-                                            <option value="English Heroes">English Heroes</option>
-                                            <option value="Content Creators">Content Creators</option>
-                                            <option value="Artwork">Artwork</option>
-                                            <option value="Projects">Projects</option>
-                                            <option value="Web">Web</option>
-                                            <option value="Public Relations">Public Relations</option>
-                                            <option value="Human Resources">Human Resources</option>
-                                            <option value="Fundraising">Fundraising</option>
-                                            <option value="Logistics">Logistics</option>
+                                            <option @if($item->first_committee=="Blender") selected @endif
+                                            value="Blender">Blender
+                                            </option>
+                                            <option @if($item->first_committee=="English Heroes") selected @endif
+                                            value="English Heroes">English Heroes
+                                            </option>
+                                            <option @if($item->first_committee=="Content Creators") selected @endif
+                                            value="Content Creators">Content Creators
+                                            </option>
+                                            <option @if($item->first_committee=="Artwork") selected @endif
+                                            value="Artwork">Artwork
+                                            </option>
+                                            <option @if($item->first_committee=="Projects") selected @endif
+                                            value="Projects">Projects
+                                            </option>
+                                            <option @if($item->first_committee=="Web") selected @endif
+                                            value="Web">Web
+                                            </option>
+                                            <option @if($item->first_committee=="Public Relations") selected @endif
+                                            value="Public Relations">Public Relations
+                                            </option>
+                                            <option @if($item->first_committee=="Human Resources") selected @endif
+                                            value="Human Resources">Human Resources
+                                            </option>
+                                            <option @if($item->first_committee=="Fundraising") selected @endif
+                                            value="Fundraising">Fundraising
+                                            </option>
+                                            <option @if($item->first_committee=="Logistics") selected @endif
+                                            value="Logistics">Logistics
+                                            </option>
                                         </select>
                                         <label for="first_committee">First Committee</label>
                                     </div>
 
                                     <div class="form-group form-md-line-input">
                                         <select required name="second_committee" class="form-control">
-                                            <option selected
-                                                    value="{{$item->second_committee}}">{{$item->second_committee}}
+                                            <option @if($item->second_committee=="Linux") selected @endif
+                                            value="Linux">Linux
                                             </option>
-                                            <option value="Linux">Linux</option>
-                                            <option value="Blender">Blender</option>
-                                            <option value="English Heroes">English Heroes</option>
-                                            <option value="Content Creators">Content Creators</option>
-                                            <option value="Artwork">Artwork</option>
-                                            <option value="Projects">Projects</option>
-                                            <option value="Web">Web</option>
-                                            <option value="Public Relations">Public Relations</option>
-                                            <option value="Human Resources">Human Resources</option>
-                                            <option value="Fundraising">Fundraising</option>
-                                            <option value="Logistics">Logistics</option>
+                                            <option @if($item->second_committee=="Blender") selected @endif
+                                            value="Blender">Blender
+                                            </option>
+                                            <option @if($item->second_committee=="English Heroes") selected @endif
+                                            value="English Heroes">English Heroes
+                                            </option>
+                                            <option @if($item->second_committee=="Content Creators") selected @endif
+                                            value="Content Creators">Content Creators
+                                            </option>
+                                            <option @if($item->second_committee=="Artwork") selected @endif
+                                            value="Artwork">Artwork
+                                            </option>
+                                            <option @if($item->second_committee=="Projects") selected @endif
+                                            value="Projects">Projects
+                                            </option>
+                                            <option @if($item->second_committee=="Web") selected @endif
+                                            value="Web">Web
+                                            </option>
+                                            <option @if($item->second_committee=="Public Relations") selected @endif
+                                            value="Public Relations">Public Relations
+                                            </option>
+                                            <option @if($item->second_committee=="Human Resources") selected @endif
+                                            value="Human Resources">Human Resources
+                                            </option>
+                                            <option @if($item->second_committee=="Fundraising") selected @endif
+                                            value="Fundraising">Fundraising
+                                            </option>
+                                            <option @if($item->second_committee=="Logistics") selected @endif
+                                            value="Logistics">Logistics
+                                            </option>
                                         </select>
                                         <label for="second_committee">Second Committee</label>
                                     </div>
 
                                     <div class="form-group form-md-line-input">
                                         <select required name="day" class="form-control">
-                                            <option selected
-                                                    value="{{$item->day}}">{{$item->day}}
+                                            <option @if($item->day=="Saturday") selected @endif
+                                            value="Saturday">Saturday
                                             </option>
-                                            <option value="Saturday">Saturday</option>
-                                            <option value="Sunday">Sunday</option>
-                                            <option value="Monday">Monday</option>
-                                            <option value="Tuesday">Tuesday</option>
-                                            <option value="Wednesday">Wednesday</option>
-                                            <option value="Thursday">Thursday</option>
+                                            <option @if($item->day=="Sunday") selected @endif
+                                            value="Sunday">Sunday
+                                            </option>
+                                            <option @if($item->day=="Monday") selected @endif
+                                            value="Monday">Monday
+                                            </option>
+                                            <option @if($item->day=="Tuesday") selected @endif
+                                            value="Tuesday">Tuesday
+                                            </option>
+                                            <option @if($item->day=="Wednesday") selected @endif
+                                            value="Wednesday">Wednesday
+                                            </option>
+                                            <option @if($item->day=="Thursday") selected @endif
+                                            value="Thursday">Thursday
+                                            </option>
                                         </select>
                                         <label for="day">Day</label>
                                     </div>
 
                                     <div class="form-group form-md-line-input">
                                         <select required name="time" class="form-control">
-                                            <option selected
-                                                    value="{{$item->time}}">{{$item->time}}
+                                            <option @if($item->time=="from 10:00 to 11:30") selected @endif
+                                            value="from 10:00 to 11:30">from 10:00 to 11:30
                                             </option>
-                                            <option value="from 10:00 to 11:30">from 10:00 to 11:30</option>
-                                            <option value="from 11:30 to 01:00">from 11:30 to 01:00</option>
-                                            <option value="from 01:00 to 02:30">from 01:00 to 02:30</option>
-                                            <option value="from 02:30 to 04:00">from 02:30 to 04:00</option>
-                                            <option value="from 04:00 to 05:30">from 04:00 to 05:30</option>
+                                            <option @if($item->time=="from 11:30 to 01:00") selected @endif
+                                            value="from 11:30 to 01:00">from 11:30 to 01:00
+                                            </option>
+                                            <option @if($item->time=="from 01:00 to 02:30") selected @endif
+                                            value="from 01:00 to 02:30">from 01:00 to 02:30
+                                            </option>
+                                            <option @if($item->time=="from 02:30 to 04:00") selected @endif
+                                            value="from 02:30 to 04:00">from 02:30 to 04:00
+                                            </option>
+                                            <option @if($item->time=="from 04:00 to 05:30") selected @endif
+                                            value="from 04:00 to 05:30">from 04:00 to 05:30
+                                            </option>
                                         </select>
                                         <label for="time">Time</label>
                                     </div>
